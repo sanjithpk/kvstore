1807c54ab392eff72730d372921bcf6d19ff6e06
Sanjith <sanjithpk06@gmail.com>
2022-01-30 17:26:33 +0530

    added database struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs, io::Error};
 
 fn main() {
     let mut arguements = std::env::args().skip(1);
@@ -7,4 +7,21 @@ fn main() {
     println!("The key is '{}' and the value is '{}'", key, value);
     let contents = format!("{}\t{}\n", key, value);
     fs::write("store.txt", contents).unwrap();
+    let database = Database::new().expect("Database new crashed");
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, Error> {
+        let mut map = HashMap::new();
+        let contents = fs::read_to_string("store.txt")?;
+        for line in contents.lines() {
+            let (key, value) = line.split_once('\t').expect("corrupt database");
+            map.insert(key.to_owned(), value.to_owned());
+        }
+        Ok(Database { map: map })
+    }
 }
